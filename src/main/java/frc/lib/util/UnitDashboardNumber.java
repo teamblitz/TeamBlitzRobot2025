a5eeeda168691d0d710191b1fb3d6592a4ce2085
Noah <40500696+NoahStricker@users.noreply.github.com>
2025-01-20 12:32:32 -0700

    United tunable dashboard numbers

--- src/main/java/frc/lib/util/UnitDashboardNumber.java
+++ src/main/java/frc/lib/util/UnitDashboardNumber.java
@@ -1,44 +1,55 @@
 package frc.lib.util;
 
+import edu.wpi.first.units.BaseUnits;
+import edu.wpi.first.units.Unit;
+import frc.lib.util.units.UnitConverter;
+
 public class UnitDashboardNumber extends LoggedTunableNumber {
-    //    private final Unit displalyUnit;
-    //
+    private final UnitConverter dashboardToInternal;
+
     /**
      * Create a new LoggedTunableNumber
      *
      * @param dashboardKey Key on dashboard
      */
     public UnitDashboardNumber(String dashboardKey) {
-        super(dashboardKey, 0);
+        this(dashboardKey, 0);
     }
-    //
-    //    public UnitDashboardNumber(String dashboardKey, double defaultValue) {
-    //        this(dashboardKey, defaultValue, BaseUnits.Value);
-    //    }
-    //
-    //    /**
-    //     * Create a new LoggedTunableNumber with a default value and display unit
-    //     *
-    //     * @param dashboardKey Key on dashboard
-    //     * @param defaultValue Default value
-    //     */
-    //    public UnitDashboardNumber(String dashboardKey, double defaultValue, Unit displalyUnit) {
-    //        super(dashboardKey, realToDisplay.applyAsDouble(defaultValue));
-    //        this.realToDisplay = realToDisplay;
-    //        this.displayToReal = displayToReal;
-    //    }
-    //
-    //    @Override
-    //    public double get() {
-    //        displalyUnit.toBaseUnits(super.get());
-    //        return displayToReal.applyAsDouble(super.get());
-    //    }
-    //
-    //    /** Builds a UnitDashboardNumber with internal unit of radians and a display unit of
-    // degrees */
-    //    public static UnitDashboardNumber radiansDegrees(String dashboardKey, double defaultValue)
-    // {
-    //        return new UnitDashboardNumber(
-    //                dashboardKey, defaultValue, Units::radiansToDegrees, Units::degreesToRadians);
-    //    }
+
+    public UnitDashboardNumber(String dashboardKey, double defaultValue) {
+        this(dashboardKey, defaultValue, BaseUnits.Value);
+    }
+
+    /**
+     * Create a new UnitDashboardNumber with SI internal units and a specified dashboard unit.
+     *
+     * @param dashboardKey Key on dashboard
+     * @param defaultValue Default value
+     * @param dashboardUnit Unit that will be displayed on the dashboard
+     */
+    public UnitDashboardNumber(String dashboardKey, double defaultValue, Unit dashboardUnit) {
+        this(
+                dashboardKey,
+                defaultValue,
+                UnitConverter.of(dashboardUnit, dashboardUnit.getBaseUnit()));
+    }
+
+    /**
+     * Create a new LoggedTunableNumber with a default value and dashboard to internal unit
+     * converter
+     *
+     * @param dashboardKey Key on dashboard
+     * @param defaultValue Default value
+     * @param dashboardToInternal Unit converter from dashboard units to internal units
+     */
+    public UnitDashboardNumber(
+            String dashboardKey, double defaultValue, UnitConverter dashboardToInternal) {
+        super(dashboardKey, dashboardToInternal.inverse().applyAsDouble(defaultValue));
+        this.dashboardToInternal = dashboardToInternal;
+    }
+
+    @Override
+    public double get() {
+        return dashboardToInternal.applyAsDouble(super.get());
+    }
 }
