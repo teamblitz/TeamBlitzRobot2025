3bfa9c38a622043449452c53cc6e090a59ba215a
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2025-03-13 17:53:05 -0600

    vision stuff initial commit

---
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.vision;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface Vision {
+
+    public class VisionInputs {
+
+        public double botInSpaceX;
+        public double botInSpaceY;
+
+        public double pixelsX;
+        public double pixelsY;
+
+        public double timestamp;
+
+        
+    
+    }
+}
