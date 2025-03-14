02dd8fca2d8b92f4bc9b708a4c6646ced44428e3
Plixet <wyatt.damiani87@gmail.com>
2025-03-14 17:54:07 -0600

    Added Necessary and Removed Redundant Inputs

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -1,11 +1,11 @@
 package frc.robot.subsystems.climber;
 
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.BlitzSubsystem;
 import frc.lib.math.EqualsUtil;
 import org.littletonrobotics.junction.Logger;
-import frc.robot.Constants.Climber;
 
 public class Climber extends BlitzSubsystem {
     private final ClimberIO io;
