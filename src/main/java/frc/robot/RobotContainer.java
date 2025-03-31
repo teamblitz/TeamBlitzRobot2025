c91ee8706632ae0d05b43a55744dc26f5a71b577
Noah <40500696+NoahStricker@users.noreply.github.com>
2025-03-31 16:55:08 -0600

    Revert "temp disable elevator and wrist"
    This reverts commit 1e86bee9ae8ef53a735f747f2c88ff190f8594a9.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -39,11 +39,9 @@ import frc.robot.subsystems.intake.IntakeIOKraken;
 import frc.robot.subsystems.intake.IntakeIOSpark;
 import frc.robot.subsystems.superstructure.Superstructure;
 import frc.robot.subsystems.superstructure.elevator.Elevator;
-import frc.robot.subsystems.superstructure.elevator.ElevatorIO;
 import frc.robot.subsystems.superstructure.elevator.ElevatorIOKraken;
 import frc.robot.subsystems.superstructure.elevator.ElevatorIOSpark;
 import frc.robot.subsystems.superstructure.wrist.Wrist;
-import frc.robot.subsystems.superstructure.wrist.WristIO;
 import frc.robot.subsystems.superstructure.wrist.WristIOKraken;
 import frc.robot.subsystems.superstructure.wrist.WristIOSpark;
 import frc.robot.subsystems.vision.Vision;
@@ -193,8 +191,8 @@ public class RobotContainer {
 
         superstructure =
                 new Superstructure(
-                        Constants.compBot() ? new ElevatorIO() {} : new ElevatorIOSpark(),
-                        Constants.compBot() ? new WristIO() {} : new WristIOSpark());
+                        Constants.compBot() ? new ElevatorIOKraken() : new ElevatorIOSpark(),
+                        Constants.compBot() ? new WristIOKraken() : new WristIOSpark());
         elevator = superstructure.getElevator();
         wrist = superstructure.getWrist();
 
