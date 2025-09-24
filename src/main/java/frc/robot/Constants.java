f97968a1aaa20aebbcb1bd2f59332f1ecefda4d1
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2025-09-23 19:44:42 -0600

    changed auto stow constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -610,7 +610,7 @@ public final class Constants {
 
     public static final class Auto {
         public static final class Timings {
-            public static final double STOW_TO_L4_READY = 1.25; // IDK ACTUAL TIMINGS
+            public static final double STOW_TO_L4_READY = 3.0; // IDK ACTUAL TIMINGS
         }
 
     }
