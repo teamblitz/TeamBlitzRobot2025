be0009ee410cb9fd225c420d3c03ad285e9fe94a
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2025-10-10 13:28:19 -0600

    Update at 'Fri Oct 10 13:28:18 MDT 2025'

--- src/main/java/frc/robot/subsystems/roller/RollerGood.java
+++ src/main/java/frc/robot/subsystems/roller/RollerGood.java
@@ -17,7 +17,7 @@ public class RollerGood extends SubsystemBase {
     public Command score() {
          return startEnd(
             () -> {
-                roller.set(1);
+                roller.set(0.5);
             },
             () -> {
                 roller.set(0);
