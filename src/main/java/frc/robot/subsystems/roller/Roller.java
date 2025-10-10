fb967ce7908a3d99a63c29431ade7ce0268e0df7
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2025-10-10 12:35:36 -0600

    Update at 'Fri Oct 10 12:35:36 MDT 2025'

--- src/main/java/frc/robot/subsystems/roller/Roller.java
+++ src/main/java/frc/robot/subsystems/roller/Roller.java
@@ -2,23 +2,38 @@ package frc.robot.subsystems.roller;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.Roller.*;
 
 /** Class to run the rollers over CAN */
 public class Roller extends SubsystemBase {
     private final RollerIO io;
 
     public Roller(RollerIO io) {
+        super("roller");
+
         this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
     }
 
     // TODO make command better
     public Command runRoller() {
-        return startEnd(
+        /*return startEnd(
                 () -> {
                     io.setSpeed(0.3);
                 },
                 () -> {
                     io.setSpeed(0);
-                });
+                });*/
+
+        return setSpeed(ROLLER_SPEED);
+    }
+
+    public Command setSpeed(double speed) {
+        return startEnd(() -> io.setSpeed(speed), () -> io.setSpeed(0));
+
     }
 }
--- src/main/java/frc/robot/subsystems/roller/RollerIO.java
+++ src/main/java/frc/robot/subsystems/roller/RollerIO.java
@@ -3,5 +3,4 @@ package frc.robot.subsystems.roller;
 public interface RollerIO {
     default void setSpeed(double speed) {}
 
-    public static double ROLLER_SPEED = 0.3;
 }
