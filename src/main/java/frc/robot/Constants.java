8feaf7c67c2ad5d345dd21f8aa25f8a9d3e873fe
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2025-10-10 11:01:47 -0600

    Update at 'Fri Oct 10 11:01:47 MDT 2025'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -157,7 +157,7 @@ public final class Constants {
         public static final double CLOSED_LOOP_RAMP = 0.0;
 
         /* Angle Motor PID Values */
-        public static final double ANGLE_KP = 0.05;
+        public static final double ANGLE_KP = 0.5;
         public static final double ANGLE_KI = 0.0;
         public static final double ANGLE_KD = 0.0;
         public static final double ANGLE_KF = 0.0; // For now, should remain zero
