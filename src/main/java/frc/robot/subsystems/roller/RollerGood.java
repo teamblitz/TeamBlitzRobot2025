e1c8eb10d921e8226d2a30345b0cfa0573d67911
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2025-10-10 12:53:05 -0600

    Update at 'Fri Oct 10 12:53:05 MDT 2025'

---
+++ src/main/java/frc/robot/subsystems/roller/RollerGood.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.roller;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.Roller.*;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class RollerGood extends SubsystemBase {
+    TalonFX roller;
+
+    public RollerGood() {
+        roller = new TalonFX(ROLLER_ID);
+    }
+
+    public Command score() {
+        return startEnd(
+            () -> {
+                roller.set(1);
+            },
+            () -> {
+                roller.set(0);
+            });
+             
+    }
+
+}
